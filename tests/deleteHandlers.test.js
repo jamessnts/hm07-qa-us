d78917ac2874ad2251d96ee1e24745901cc595f6
James Santos <pro.santosvincent@gmail.com>
2024-02-08 01:45:02 -0600

    Project 7 First Submission 1.3

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
-
 test('Should return code 200', async () => {
 	let actualStatusCode;
     try {
@@ -14,18 +13,15 @@ test('Should return code 200', async () => {
 	expect(actualStatusCode).toBe(200);
 });
 
-test('Response Body should be empty', async () => {
+test('Response Body should contain...', async () => {
 	let actualResponseBody;
     try {
 		const response = await fetch(`${config.API_URL}/api/v1/orders/12`, {
 			method: 'DELETE',
 		});
-	
-		if (response.status !== 204) {
-			actualResponseBody = await response.json();
-		}
+		actualResponseBody = await response.json();
 	} catch (error) {
 		console.error(error);
 	}
-	expect(actualResponseBody).toBeUndefined();
+	expect(actualResponseBody["ok"]).toBe("true")
 });
